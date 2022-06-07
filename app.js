f333d7775ae9202660839601276d7cbac7279ad5
abiha456 <abiha.amir456@gmail.com>
2022-06-07 01:32:54 -0700

    abcd

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-
+const port = process.env.PORT;
 var routing = require('./routes/router.js');
-// ...
 app.use('/', routing);
-app.listen(3001,function(){
+app.listen(port,function(){
     
     console.log("server start")
 })
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,9 @@
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://faraz:Adobe110@cluster0.i9hdb.mongodb.net/?retryWrites=true&w=majority";
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+const {dbConnection} = require('../database/connection.js')
+// const uri = "mongodb+srv://faraz:Adobe110@cluster0.i9hdb.mongodb.net/?retryWrites=true&w=majority";
 // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
  
 
 exports.index = (req, res) => {
@@ -35,26 +36,22 @@ exports.add = (req, res) => {
     res.render("add");
 };
 exports.insert = (req, res) => {
-    console.log("hello world")
-    client.connect(async(err) => {
-        if(err) throw err;
-            const userCollections = client.db('myDB').collection('productCollection')
-            await userCollections.insertOne(req.body,(err,result)=>{
-                if (err) throw err;
-                    if(result.acknowledged){
-                        console.log("add")
-                        res.render("add");
-                    }
-                    else{
-                        res.render("add");
-                        console.log("not insert")
-                        // res.render("login");
-                    }
+    
+    dbConnection().then(function(client){
+        const userCollections = client.db('myDB').collection('ProductCollection')
+        // await userCollections.insertOne({name:"faraz"},(err,result)=>{
+        //     if (err) throw err;
+        //         if(result.acknowledged){
+        //             console.log("add")
+        //             // res.render("login");
+        //         }
+        //         else{
+        //             console.log("not insert")
+        //             // res.render("login");
+        //         }
 
-            })
-            // client.close()
-
-    });
+        // })
+    })
 };
 exports.edit = (req, res) => {
     res.render("edit");
