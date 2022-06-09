38bf890aac0e44a28428105f14b717cc70403c9e
abiha456 <abiha.amir456@gmail.com>
2022-06-09 01:18:15 -0700

    changes

---
+++ models/product.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+});
+
+const productDB = mongoose.model("products", productSchema);
+export { productDB };
