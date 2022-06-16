bb8bc3b916e882b64a5ebf51d3753bf4801b5ea8
abiha456 <abiha.amir456@gmail.com>
2022-06-16 01:32:28 -0700

    react

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require("express");
+var cors = require('cors')
 var path = require("path");
 var bodyParser = require("body-parser");
 var app = express();
 const dotenv = require("dotenv");
 // const dbConnection  = require("./database/mongooseConnection.js");
 dotenv.config();
+app.use(cors())
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
